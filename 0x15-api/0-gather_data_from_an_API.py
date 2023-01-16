222b095f9dc1a5778ffb8fd9e8e69ba05b95ddc9
radar2 <securkydroity@gmail.com>
2023-01-16 23:52:57 +0000

    Get Api data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,10 +10,10 @@ def get_todo():
     """
     user_id = int(sys.argv[1])
     user_result = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".format(user_id))
+        'https://jsonplaceholder.typicode.com/users/{}'.format(user_id))
     user_data = user_result.json()
     to_do_result = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
             user_data['id']))
     to_do_list = to_do_result.json()
     complete_count = 0
@@ -23,10 +23,10 @@ def get_todo():
             complete_count = complete_count + 1
         else:
             uncompleted_count = uncompleted_count + 1
-    print("Employee {} is done with tasks({}/{}):".format(
+    print('Employee {} is done with tasks({}/{}):'.format(
         user_data['name'], complete_count, len(to_do_list)))
     for todo in to_do_list:
-        print("\t {}".format(todo['title']))
+        print('\t {}'.format(todo['title']))
 
 
 if __name__ == '__main__':
