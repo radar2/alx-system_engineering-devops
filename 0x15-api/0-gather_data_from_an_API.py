17c96022e1d363b30a54725a9c10d2f565341caa
radar2 <securkydroity@gmail.com>
2023-01-16 23:36:08 +0000

    Get Api data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Fetch informations of todo list"""
-import urllib.request
-import  requests
+import requests
 import sys
 
 
@@ -9,24 +8,24 @@ def get_todo():
     """
     List to do list
     """
-    id = int(sys.argv[1])
-    try:
-        user_result = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(id))
-        user_data = user_result.json()
-        to_do_result = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".format(user_data['id']))
-        to_do_list = to_do_result.json()
-        complete_count = 0
-        uncompleted_count = 0
-        for todo in to_do_list:
-            if todo['completed']:
-                complete_count = complete_count + 1
-            else:
-                uncompleted_count = uncompleted_count + 1
-        print("Employee {} is done with tasks({}/{}):".format(user_data['name'], complete_count, len(to_do_list)))
-        for todo in to_do_list:
-            print("\t {}".format(todo['title']))
-    except:
-        print("Error")
+    user_id = int(sys.argv[1])
+    user_result = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(user_id))
+    user_data = user_result.json()
+    to_do_result = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_data['id']))
+    to_do_list = to_do_result.json()
+    complete_count = 0
+    uncompleted_count = 0
+    for todo in to_do_list:
+        if todo['completed']:
+            complete_count = complete_count + 1
+        else:
+            uncompleted_count = uncompleted_count + 1
+    print("Employee {} is done with tasks({}/{}):".format(
+        user_data['name'], complete_count, len(to_do_list)))
+    for todo in to_do_list:
+        print("\t {}".format(todo['title']))
 
 
 if __name__ == '__main__':
