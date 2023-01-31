c83830da2f9ec1ad744ea98ca63545e894fb4c5c
radar2 <securkydroity@gmail.com>
2023-01-31 18:45:08 +0000

    Updazte

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,14 +5,13 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns total subs"""
-    header = {"User-Agent": "Holberton"}
+    header = {"User-Agent": "ALX-SCHOOL"}
     url = "https://www.reddit.com/r/" + subreddit + "/about.json"
     r = requests.get(url, headers=header)
     if r.status_code == 200:
         return r.json().get("data", None).get("subscribers", None)
     else:
-        return(0)
-
+        return (0)
 
 if __name__ == "__main__":
     pass
