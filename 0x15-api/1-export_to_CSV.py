c52b82950c9d1078cf1634889ed90c96796c220c
radar2 <securkydroity@gmail.com>
2023-05-26 01:13:43 +0000

    export to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,29 +1,38 @@
 #!/usr/bin/python3
-"""Fetch informations of todo list"""
+"""
+Fetch information's of todo list
+"""
 import requests
 import sys
+import csv
 
 
 def get_todo():
-    """
-    List to do list
+    """List to do list
     """
     user_id = int(sys.argv[1])
     user_result = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}'.format(user_id))
     user_data = user_result.json()
+    username = user_data['username']
     to_do_result = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
             user_data['id']))
     to_do_list = to_do_result.json()
-    completed_list = []
+    rows = []
     for todo in to_do_list:
-        if todo['completed']:
-            completed_list.append(todo)
-    print('Employee {} is done with tasks({}/{}):'.format(
-        user_data['name'], len(completed_list), len(to_do_list)))
-    for todo in completed_list:
-        print('\t {}'.format(todo['title']))
+        row = [user_id, username, todo['completed'], todo['title']]
+        rows.append(row)
+
+    filename = str(user_id) + '.csv'
+    export_csv(filename, rows)
+
+
+def export_csv(filename, rows):
+    with open(filename, 'w', encoding='UTF-8', newline='') as f:
+        writer = csv.writer(f, dialect='excel')
+        writer.writerows(rows)
+        f.close()
 
 
 if __name__ == '__main__':
